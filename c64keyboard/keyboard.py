d9bc82f349dc105e6aded3123dd7db6a8285921d
aceone <henols@gmail.com>
2024-04-18 11:20:53 +0200

    First commit

---
+++ c64keyboard/keyboard.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+
+from PIL import ImageTk, Image
+
+import time
+import argparse
+import serial
+import serial.tools.list_ports
+import re
+import json
+
+import sys
+import termios
+import os
+
+
+LINE_PREFIX = "CommadLine:"
+LOAD_8 = LINE_PREFIX + "load" + ("{CURSOR_RIGHT}") * 19 + ",8:{RETURN}"
+LOAD_DIR = LINE_PREFIX + 'load"$",8:{RETURN}'
+LOAD_ACEONE = LINE_PREFIX + 'load"http://c64.aceone.se",8:{RETURN}'
+LOAD_MEATLOAF = LINE_PREFIX + 'load"ml:*",8:{RETURN}'
+
+HEX_PREFIX = "0x"
+BAUD = 19200
+
+serialDevice = None
+
+
+def get_matrix_value(c):
+    return key_matrix.get(c, -1)
+
+
+def get_special_value(c):
+    return special_keys.get(c, -1) | 0x40 if c in special_keys else -1
+
+
+def get_special_release_value(c):
+    return special_release_keys.get(c, "")
+
+
+def combination_to_matrix(c, pressed):
+    values = bytearray()
+    for l in c.split("|"):
+        if l.endswith("_OFF") and pressed:
+            matrix_val = False
+            l = l[:-4]
+        else:
+            matrix_val = pressed
+
+        value = get_matrix_value(l)
+        if value < 0:
+            value = get_special_value(l)
+        #        print(f"cmd: {l}, pressed: {matrix_val}")
+        if value >= 0 and value not in values:
+            values.append(value | 0x80 if matrix_val else value)
+
+    return values
+
+
+def build_key_combination(c, pressed=True):
+    key_combo = ""
+    if get_matrix_value(c) >= 0:
+        key_combo = ("SHIFT_LEFT_OFF|" if c in no_shift else "") + c
+    elif len(c) == 1 and re.match("[A-ZÅÄÖ]", c):
+        key_combo = "SHIFT_LEFT|" + c.lower()
+    elif c in key_mappings:
+        key_combo = key_mappings[c]
+
+    if key_combo == "LOAD_DIR":
+        key_combo = LOAD_DIR
+
+    if not pressed:
+        value = get_special_release_value(c)
+        if value:
+            key_combo = key_combo + "|" + value
+
+    return key_combo
+
+
+def parse_key_combinations(key_combo, pressed=True):
+    values = combination_to_matrix(key_combo, pressed)
+
+    if values:
+        print(f"key combination: {key_combo}")
+        for val in values:
+            img = key_imgs.get(val & 0x7F, None)
+            if img:
+                s = "normal" if val & 0x80 else "hidden"
+                canvas.itemconfig(img["id"], state=s)
+            if val & 0xC3 == 0xC3:
+                for img in key_imgs.values():
+                    canvas.itemconfig(img["id"], state="hidden")
+        hex_string = " ".join(f"{HEX_PREFIX}{element:02X}" for element in values)
+        print(f"values: {hex_string}")
+        if not args.dummy:
+            written = arduino.write(bytearray([len(values)]))
+            written = arduino.write(values)
+            arduino.flush()
+        print("------------ Sent-----------------------")
+    else:
+        print(f"Unknown key combination: {key_combo}")
+        print("----------------------------------------")
+
+
+def process_key(c, pressed=True):
+    hex_string = ""
+    if re.match("[\\w!@#$%^&*()-_=+\\\\|,<.>/?`~\\[\\]{}\"\\']", c):
+        print(f"'{c}' in hex = 0x")
+    else:
+        for element in c:
+            hex_string += f"0x{ord(element):02X} "
+    print(f"other key: {hex_string}")
+
+    key_combo = build_key_combination(c, pressed)
+    if key_combo.startswith(LINE_PREFIX):
+        if not pressed:
+            return
+        parse_key_combinations("TEXT", True)
+        for m in re.finditer(r"(\{(\w+)\})|.", key_combo[len(LINE_PREFIX) :]):
+            # print (m.group().strip("{}"))
+            key_combo = build_key_combination(m.group().strip("{}"))
+            parse_key_combinations(key_combo)
+            time.sleep(0.05)
+        parse_key_combinations("TEXT", False)
+    else:
+        parse_key_combinations(key_combo, pressed)
+
+
+def decode_key(event):
+    if event.keysym_num >= 33 and event.keysym_num <= 126:
+        key = chr(event.keysym_num)
+    elif event.keysym == "??":
+        key = event.char
+    else:
+        key = event.keysym
+    return key
+
+
+def on_key_event(event, pressed):
+    key = decode_key(event)
+    print(f"Key {'pressed' if pressed else 'released'}: {key}")
+    process_key(key, pressed)
+
+
+def read_input():
+    if not args.dummy:
+        while True:
+            line = arduino.readline()
+            if len(line.strip()) > 0:
+                data = line.decode("utf-8").strip()
+                print(f" --> {data}")
+            else:
+                break
+    window.after(200, read_input)
+
+
+def handle_focus(event):
+    if event.widget == window:
+        parse_key_combinations("RESET_MATRIX")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="key64tapper, a C64 keyboard emulator."
+    )
+    specifyDevices = parser.add_mutually_exclusive_group(required=True)
+    specifyDevices.add_argument(
+        "-l",
+        "--list",
+        action="store_true",
+        help="List available USB devices and exit. Use this option to find a device for the -d / --device option.",
+    )
+    specifyDevices.add_argument(
+        "-d",
+        "--device",
+        action="store",
+        type=str,
+        dest="usbDevice",
+        help="Specify an Arduino-like USB device to use.",
+    )
+    specifyDevices.add_argument(
+        "-D",
+        "--dummy",
+        action="store_true",
+        help="Dummy mode. Don't connect to a device, print all serial output to STDOUT instead.",
+    )
+    parser.add_argument(
+        "-r",
+        action="store",
+        type=str,
+        dest="rawString",
+        required=False,
+        help="User-supplied string of keys to send.",
+    )
+    parser.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="Interactive mode. Whatever you type is passed on to Commodore.",
+    )
+    return parser.parse_args()
+
+
+def load_config():
+    global key_matrix, layout, no_shift, special_release_keys, special_keys, key_mappings, c64_type, lang
+    c64_type = "breadbin"
+    lang = "_sv"
+    # lang =""
+
+    key_layout = json.load(open(f"config/keyboard_matrix{lang}.json"))
+    layout = key_layout["layout"]
+    print(f"Using key map: {layout}")
+    key_matrix = key_layout["matrix"]
+    no_shift = key_layout["no-shift"]
+    
+    special_release_keys = {}
+    if "special-release-keys" in key_layout:
+        special_release_keys = key_layout["special-release-keys"]
+
+    key_config = json.load(open(f"config/key_config.json"))
+    special_keys = key_config["special-keys"]
+    key_mappings = key_config["key-mappings"]
+    key_mappings.update(key_layout["key-mappings"])
+
+
+def donothing():
+    x = 0
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    if args.list:
+        print(
+            "\n".join(
+                f"Device: {p.device} ; Description: {p.description}"
+                for p in serial.tools.list_ports.comports()
+            )
+        )
+        sys.exit()
+
+    serialDevice = args.usbDevice
+
+    if not args.dummy:
+        try:
+            print(f"Using device: {serialDevice}")
+            arduino = serial.Serial(serialDevice, BAUD, timeout=0.1, write_timeout=0.1)
+            # arduino = serial.Serial(serialDevice, BAUD)
+        except:
+            print("Cannot open serial device, exiting.")
+            sys.exit()
+        # Give serial interface time to settle down
+        time.sleep(1)
+
+    load_config()
+
+    # Create the GUI window
+    window = tk.Tk()
+    window.resizable(False, False)
+    window.title(f"C64 Keyboard, {layout} layout")
+    window.geometry("1006x290")
+    window.bind("<KeyPress>", lambda e: on_key_event(e, True))
+    window.bind("<KeyRelease>", lambda e: on_key_event(e, False))
+    window.bind("<FocusIn>", handle_focus)
+
+    menubar = tk.Menu(window)
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Open", command=donothing)
+    filemenu.add_command(label="Configure", command=donothing)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=window.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
+
+    helpmenu = tk.Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="Paste", command=donothing)
+    menubar.add_cascade(label="Edit", menu=helpmenu)
+
+    window.config(menu=menubar)
+
+    canvas = tk.Canvas(window, height=290, width=1006)
+    bgImg = ImageTk.PhotoImage(Image.open(f"images/{c64_type}_keyboard{lang}.png"))
+    kb = canvas.create_image(0, 0, anchor=tk.NW, image=bgImg)
+    canvas.pack()
+
+    # returns JSON object as
+    # a dictionary
+    keys_info = json.load(open(f"config/keyboard_layout/{c64_type}{lang}.json"))
+
+    # # canvas = tk.Canvas(window, height=250, width=300)
+    key_imgs = {}
+    for detail in keys_info:
+        img = ImageTk.PhotoImage(Image.open(detail["filename"]))
+        id = canvas.create_image(
+            detail["x"], detail["y"], anchor=tk.NW, image=img, state="hidden"
+        )
+        key_imgs[detail["matrix_pos"]] = {"img": img, "id": id}
+
+    window.update()
+
+    window.after(20, read_input)
+    # Run the GUI event loop
+    window.mainloop()
+    print("Exiting...")
+    sys.exit()
+
+
+# def save_square(img, x, y, w, h, c64_type, key_text, layout):
+#     filename = f"images/keys/{c64_type}_key_{key_text}{layout}.jpg"
+#     square_img = img[y : y + h, x : x + w]
+#     # if os.path.exists(filename = f"images/keys/{c64_type}_key_{key_text}.jpg"):
+#     #     excisting = cv2.imread(filename)
+#     #     # compute difference
+#     #     difference = cv2.subtract(square_img, excisting)
+#     #     print(f"Diff: {np.sum(difference)}")
+
+#     cv2.imwrite(filename, square_img)
+#     return filename
---
+++ tools/generate_layouts.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+import json
+import os
+import unicodedata
+
+
+def find_squares(img):
+    squares = []
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Enhance contrast by thresholding
+    _, thresh = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY_INV)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        cnt_len = cv2.arcLength(cnt, True)
+        cnt = cv2.approxPolyDP(cnt, 0.02 * cnt_len, True)
+        if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+            squares.append(cnt)
+    return squares
+
+
+def get_square_position(square):
+    x, y, w, h = cv2.boundingRect(square)
+    return x, y, w, h
+
+
+def save_square(img, x, y, w, h, c64_type, key_text, layout):
+    filename = f"images/keys/{c64_type}_key_{key_text}.png"
+    square_img = img[y : y + h, x : x + w]
+    if os.path.exists(filename):
+        excisting = cv2.imread(filename)
+        if (
+            excisting.size == square_img.size
+            and np.sum(cv2.subtract(square_img, excisting)) == 0
+        ):
+            return filename, False
+        filename = f"images/keys/{c64_type}_key_{key_text}{layout}.png"
+    print(f"Saving {filename}")
+    cv2.imwrite(filename, square_img)
+    return filename, True
+
+
+class Rectangle:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def find_point_in_rectangle(rect, points):
+    """Finds the first point that lies within the given rectangle."""
+    for point in points:
+        if (
+            rect.x <= point.x <= rect.x + rect.w
+            and rect.y <= point.y <= rect.y + rect.h
+        ):
+            return point
+    return None  # Return None if no point fits in the rectangle
+
+
+def load_config(layout):
+    keyboard_matrix = json.load(open(f"config/keyboard_matrix{layout}.json"))
+    print(f"Loading config for {keyboard_matrix['layout']} layout.")
+    key_matrix = keyboard_matrix["matrix"]
+
+    key_positions = {}
+    for key in json.load(open(f"tools/layout_key_pos{layout}.json")):
+        key_positions[Point(key["x"] + 10, key["y"] + 10)] = key["text"]
+
+    special_keys = json.load(open(f"config/key_config.json"))["special-keys"]
+    return key_matrix, key_positions, special_keys
+
+
+if __name__ == "__main__":
+    c64_types = ["breadbin", "c64c"]
+    layouts = [
+        "us",
+        "sv",
+    ]
+
+    for layout in layouts:
+        layout = "_" + layout if layout != "us" else ""
+        key_matrix, key_positions, special_keys = load_config(layout)
+        for c64_type in c64_types:
+            pressed_keys = f"images/{c64_type}_pressed{layout}.png"
+            if not os.path.exists(pressed_keys):
+                continue
+            img = cv2.imread(pressed_keys)
+
+            squares = find_squares(img)
+
+            square_details = []
+            saved_files = 0
+            for i, square in enumerate(squares):
+                x, y, w, h = get_square_position(square)
+                point = find_point_in_rectangle(
+                    Rectangle(x, y, w, h), key_positions.keys()
+                )
+
+                text = key_positions.get(point)
+                matrix_pos = (
+                    key_matrix[text]
+                    if text in key_matrix
+                    else special_keys[text] | 0x40
+                )
+
+                key_text = ""
+                for s in text:
+                    if s.isalnum() or s == "_":
+                        key_text += s
+                    else:
+                        key_text += unicodedata.name(s).replace(" ", "_")
+                key_text = key_text.lower()
+                filename, saved = save_square(img, x, y, w, h, c64_type, key_text, layout)
+                if saved:
+                    saved_files += 1
+                # text = preprocess_image_for_ocr(filename)
+                square_details.append(
+                    {
+                        "filename": filename,
+                        "x": x,
+                        "y": y,
+                        "w": w,
+                        "h": h,
+                        # "point": {"x": point.x, "y": point.y},
+                        "text": text,
+                        "matrix_pos": matrix_pos,
+                    }
+                )
+
+            json.dump(
+                square_details,
+                open(f"config/keyboard_layout/{c64_type}{layout}.json", "w"),
+                indent=4,
+            )
+            print(f"Saved {saved_files} keys for {c64_type} {layout}")
